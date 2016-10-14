203110361d83edb308432d0dd1516e0a36449ddf
Wilson Giese <giese.wilson@gmail.com>
2016-10-13 23:08:36 -0700

    lol

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use std::process::Command;
+
 fn main() {
-    println!("Hello, world!");
+    Command::new("osascript")
+            .arg("-e")
+            .arg("tell application \"System Events\" to keystroke \"cargo run\n\"")
+            .output();
 }
