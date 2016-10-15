87de9aded82ee2752f81a3351b70dddc45af5c8a
Wilson Giese <giese.wilson@gmail.com>
2016-10-14 22:11:10 -0700

    Activate application

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,20 @@
 use std::process::Command;
 
 fn main() {
+    activate_application("TextEdit");
+    send_string_as_keystrokes("Hello, TextEdit!");
+}
+
+fn send_string_as_keystrokes(s: &str) {
     Command::new("osascript")
             .arg("-e")
-            .arg("tell application \"System Events\" to keystroke \"cargo run\n\"")
+            .arg(format!("tell application \"System Events\" to keystroke \"{}\"", s))
+            .output();
+}
+
+fn activate_application(name: &str) {
+    Command::new("osascript")
+            .arg("-e")
+            .arg(format!("activate application\"{}\"", name))
             .output();
 }
