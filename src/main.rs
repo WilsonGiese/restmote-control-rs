18aca1a8a2f907dcf99f67a5c91b77ff2fcd833e
Wilson Giese <giese.wilson@gmail.com>
2016-10-17 00:23:59 -0700

    Add a little command line arg handling

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,35 @@ use std::process::Command;
 
 
 fn main() {
-    activate_application("TextEdit");
-    send_string_as_keystrokes("Hello, TextEdit!");
+    let args: Vec<String> = env::args().collect();
+    let program = args[0].clone();
+
+    let mut opts = Options::new();
+    opts.optopt("t", "target", "set target application name", "NAME");
+    let matches = match opts.parse(&args[1..]) {
+        Ok(m) => { m }
+        Err(f) => {
+            println!("Error: {}", f);
+            print_usage(&program, opts);
+            return;
+        }
+    };
+    
+    let target = match matches.opt_str("t") {
+        Some(t) => t,
+        None    => {
+            print_usage(&program, opts);
+            return;
+        }
+    };
+
+    activate_application(&target);
+    send_string_as_keystrokes(&format!("Hello, {}!", &target));
+}
+
+fn print_usage(program: &str, opts: Options) {
+    let brief = format!("Usage: {} FILE [options]", program);
+    print!("{}", opts.usage(&brief));
 }
 
 fn send_string_as_keystrokes(s: &str) {
