32d585dc86d2b5820d90c458b521472f4e3964e8
Wilson Giese <giese.wilson@gmail.com>
2016-10-22 22:32:42 -0700

    Take pid as param

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,9 @@ use core_graphics::event_source::{CGEventSource,CGEventSourceStateID};
 extern crate getopts;
 use getopts::Options;
 
+extern crate libc;
+use libc::pid_t;
+
 use std::env;
 use std::process::Command;
 
@@ -15,7 +18,7 @@ fn main() {
     let program = args[0].clone();
 
     let mut opts = Options::new();
-    opts.optopt("t", "target", "set target application name", "NAME");
+    opts.optopt("p", "pid", "set target pid were keyboard events will be sent", "<PROCESS ID>");
     let matches = match opts.parse(&args[1..]) {
         Ok(m) => { m }
         Err(f) => {
@@ -25,38 +28,39 @@ fn main() {
         }
     };
 
-    let target = match matches.opt_str("t") {
-        Some(t) => t,
-        None    => {
+    let pid = match matches.opt_str("p") {
+        Some(p) => {
+            match p.parse::<pid_t>() {
+                Ok(p) => p,
+                Err(e) => panic!("{}", e)
+            }
+        },
+        None => {
             print_usage(&program, opts);
             return;
         }
     };
 
-    // Send "Hello!""
-    for i in 0..2 {
-        send_keyboard_event(0x12, true);
-        send_keyboard_event(0x12, false);
-        // Sleep because if we don't the next event post seems to be missed, unless we post a lot
-        // events, then they don't get missed, or most of them don't get missed. This API is kind
-        // of buggy... Fun -_-
-        thread::sleep_ms(25);
-        send_keyboard_event_with_flags(0x12, CGEventFlags::Shift, true);
-        send_keyboard_event_with_flags(0x12, CGEventFlags::Shift, false);
-        thread::sleep_ms(25);
-    }
+    press_key(pid, 0x04); // h
+    press_key(pid, 0x0E); // e
+    press_key(pid, 0x25); // l
+    press_key(pid, 0x25); // l
+    press_key(pid, 0x1F); // o
+    press_key(pid, 0x34); // ENTER (\n)
 }
 
-fn print_usage(program: &str, opts: Options) {
-    let brief = format!("Usage: {} FILE [options]", program);
-    print!("{}", opts.usage(&brief));
+fn press_key(pid: pid_t, keycode: CGKeyCode) -> Result<(), ()> {
+
+    try!(send_keyboard_event(pid, keycode, true));
+    send_keyboard_event(pid, keycode, false)
 }
 
-fn send_keyboard_event(keycode: CGKeyCode, keydown: bool) -> Result<(), ()> {
+fn send_keyboard_event(pid: pid_t, keycode: CGKeyCode, keydown: bool) -> Result<(), ()> {
+    thread::sleep_ms(10);
     let eventSource = try!(CGEventSource::new(CGEventSourceStateID::HIDSystemState));
     let event = try!(CGEvent::new(eventSource, keycode, keydown));
-    event.post_to_pid(1645);
-    
+    event.post_to_pid(pid);
+
     Ok(())
 }
 
@@ -64,7 +68,12 @@ fn send_keyboard_event_with_flags(keycode: CGKeyCode, flags: CGEventFlags, keydo
     let eventSource = try!(CGEventSource::new(CGEventSourceStateID::HIDSystemState));
     let event = try!(CGEvent::new(eventSource, keycode, keydown));
     event.set_flags(flags);
-    event.post_to_pid(1645);
+    event.post_to_pid(247);
 
     Ok(())
+}
+
+fn print_usage(program: &str, opts: Options) {
+    let brief = format!("Usage: {} FILE [options]", program);
+    print!("{}", opts.usage(&brief));
 }
