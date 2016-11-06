97ffcc289eec34e5d794d25b0608dad8fc7da33b
Wilson Giese <giese.wilson@gmail.com>
2016-11-05 22:13:19 -0700

    Remove old main

--- src/main.rs
+++ src/main.rs
@@ -61,56 +61,6 @@ fn main() {
     server::run(pid, delay)
 }
 
-// fn main() {
-//     let args: Vec<String> = env::args().collect();
-//     let program = args[0].clone();
-//
-//     // Setup program opts
-//     let mut opts = Options::new();
-//     opts.optopt("p", "pid", "set target pid where keyboard events will be sent", "<PROCESS ID>");
-//     opts.optopt("d", "delay", "set the delay between up & down key presses in ms", "<DELAY MS>");
-//     let matches = match opts.parse(&args[1..]) {
-//         Ok(m) => m,
-//         Err(e) => {
-//             print_usage(&program, opts, Some(&e));
-//             return;
-//         }
-//     };
-//
-//     // Parse out -p --pid flag
-//     let pid = match matches.opt_str("p") {
-//         Some(p) => {
-//             match p.parse::<pid_t>() {
-//                 Ok(p) => p,
-//                 Err(e) => {
-//                     print_usage(&program, opts, Some(&e));
-//                     return;
-//                 }
-//             }
-//         },
-//         None => {
-//             print_usage(&program, opts, None);
-//             return;
-//         }
-//     };
-//
-//     // Parse out -d --delay flag
-//     let delay = match matches.opt_str("d") {
-//         Some(d) => {
-//             match d.parse::<u64>() {
-//                 Ok(d) => d,
-//                 Err(e) => {
-//                     print_usage(&program, opts, Some(&e));
-//                     return;
-//                 }
-//             }
-//         },
-//         None => DEFAULT_KEYPRESS_DELAY,
-//     };
-//
-//     server::run(pid, delay);
-// }
-
 // Print a message and error then exit with status 1
 fn error(msg: &str, error: &Display) {
     println!("{}: {}", msg, error);
